33af2b6d3e48ec26be2282ea9c5fc1dff7993a71
Andy Lushman <andylushman@gmail.com>
2017-07-09 16:20:58 -0600

    Changed error message for geolocation

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -59,7 +59,7 @@ function initMap() {
   function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
     infoWindow.setContent(browserHasGeolocation ?
-      'Error: The Geolocation service failed.' :
+      'Oh snap: The Geolocation service failed.' :
       "Error: Your browser doesn't support geolocation.");
     infoWindow.open(map);
   } //end handleLocationError()
